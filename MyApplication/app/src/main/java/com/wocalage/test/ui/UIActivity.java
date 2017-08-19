3d5d5b99f1a841a0603dd313836ea5eef63ab3bf
wocalage <1650507438@qq.com>
2017-08-19 14:35:10 +0800

    add ui file
    rt

--- MyApplication/app/src/main/java/com/wocalage/test/ui/UIActivity.java
+++ MyApplication/app/src/main/java/com/wocalage/test/ui/UIActivity.java
@@ -1,10 +1,16 @@
 package com.wocalage.test.ui;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by jiaojian on 2017/8/18.
  */
 
 public class UIActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
