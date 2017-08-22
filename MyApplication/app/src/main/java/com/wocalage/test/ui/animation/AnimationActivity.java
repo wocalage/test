7f729bc26b3d87fabf46a7a5069dda97b311b2bf
wocalage <1650507438@qq.com>
2017-08-22 22:12:07 +0800

    add ui modle item activity announce
    rt

--- MyApplication/app/src/main/java/com/wocalage/test/ui/animation/AnimationActivity.java
+++ MyApplication/app/src/main/java/com/wocalage/test/ui/animation/AnimationActivity.java
@@ -1,8 +1,10 @@
 package com.wocalage.test.ui.animation;
 
+import android.app.Activity;
+
 /**
  * Created by jiaojian on 2017/8/22.
  */
 
-public class AnimationActivity {
+public class AnimationActivity extends Activity{
 }
