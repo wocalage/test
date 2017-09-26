8884e371ebeeeed56bcdc399e3255841d5871752
wocalage <1650507438@qq.com>
2017-09-26 17:24:42 +0800

    add some anim part
    rt

--- MyApplication/app/src/main/java/com/wocalage/test/ui/animation/AnimationActivity.java
+++ MyApplication/app/src/main/java/com/wocalage/test/ui/animation/AnimationActivity.java
@@ -1,10 +1,19 @@
 package com.wocalage.test.ui.animation;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.wocalage.test.R;
 
 /**
  * Created by jiaojian on 2017/8/22.
  */
 
 public class AnimationActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.model_ui_animation);
+    }
 }
---
+++ MyApplication/app/src/main/java/com/wocalage/test/ui/animation/FrameView.java
@@ -0,0 +1,20 @@
+package com.wocalage.test.ui.animation;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jiaojian on 2017/9/26.
+ */
+
+public class FrameView extends View {
+
+    public FrameView(Context context) {
+        super(context);
+    }
+
+    public FrameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
